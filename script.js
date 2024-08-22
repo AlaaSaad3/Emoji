da475029b882038a0a239ca039dce356549e7733
Alaa Saad <alaa1.saed71@gmail.com>
2024-08-22 17:01:12 +0300

    Emoji

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn=document.querySelector("#emoji");
+
+const emojis=[
+    "😀","😁","😂","🤣","😃","😆","😉","😎","😍","😘","🤩","🤐","😴","🤑","🤯"
+];
+
+btn.addEventListener("mouseover",()=>{
+    btn.innerHTML=emojis[Math.floor(Math.random()*emojis.length)];
+})
